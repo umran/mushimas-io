16f8fcb7cf7a945321a1483c4a9b3ace49dd01df
Umran Hussein <umran.hussein@gmail.com>
2019-05-01 12:25:12 -0700

    implemented proper search indexing

--- src/search/upsert.js
+++ src/search/upsert.js
@@ -2,24 +2,26 @@ const model = require('../model')
 
 const PARENT_PATH = '@document'
 
-const convertProjection = projection => {
-  return Object.keys(projection).reduce((converted, key) => {
-    converted[`${PARENT_PATH}.${key}`] = projection[key]
+const extractDoc = (doc, projection) => {
+  return Object.keys(projection).reduce((result, field) => {
+    if (projection[field] === 1) {
+      result[field] = doc['@document'][field]
+    }
 
-    return converted
+    return result
   }, {})
 }
 
 module.exports = async ({context, projection, _id, client}) => {
   const { bucket, collection } = context
-  let doc = await model.findOne({ _id, '@collection': collection, '@bucket': bucket }, { ...convertProjection(projection), _id: 0 }).lean()
+  let doc = await model.findOne({ _id, '@collection': collection, '@bucket': bucket }).lean()
 
   await client.update({
     index: `${bucket}_${collection}`,
     type: `${bucket}_${collection}`,
     id: _id,
     body: {
-      doc,
+      doc: extractDoc(doc, projection),
       doc_as_upsert: true
     }
   })
